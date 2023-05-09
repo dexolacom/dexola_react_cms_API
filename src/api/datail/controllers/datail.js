33dc2b644674070b24f9eca414a7f43b5c1fcae3
zAndrey95 <zahorulko.andrew@gmail.com>
2023-05-09 15:35:37 +0300

    UPD | new version

--- src/api/datail/controllers/datail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * datail controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::datail.datail');
--- src/api/datail/routes/datail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * datail router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::datail.datail');
